29d87a411973ce0316e5a50c7c5a0a09bdbb4e63
Vigilancia Epidemiológica <Vigilancia Epidemiolgica>
2016-10-10 22:29:10 -0500

    'funcionando'

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+	include('class/classAsistencias.php');
+	$clase = new sistema;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Registro de Mascotas</title> 
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="css/estilos.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+<link rel="stylesheet" type="text/css" href="css/calendario.css">
+<script src="js/jquery.js"></script>
+<script src="js/myjava.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+</head>
+
+
+<body>
+<section class="sectionI">
+    
+    <br />
+    		<center>
+            		<img src="img/banner.png" height="250" />
+            </center>
+            
+            <br />
+            <br />
+            <br />
+    		<center>
+    		<tr>
+                    <td>
+                    <a href="indexmascotas.php"><img src="img/registro.png" height="35" /></a>
+                    </td>
+                    <td>
+                    <a href="indexprod.php"><img src="img/reportes.png" height="35" /></a>
+                    </td>
+            		<td>
+                    <a href="indexcliente.php"><img src="img/derechos.png" height="35" /></a>
+                    </td>
+                    <td>
+                    <a href="deberes.php"><img src="img/deberes.png" height="35" /></a>
+                    </td>
+                    
+            </tr>
+            </center>
+            
+            <br/>
+            <br/>
+            
+        <img src="img/developed.png" height="15" />    
+    
+    <br />
+    
+    
+    </section>
+    </body>
+    </html>
+    
